9c4c206c53983f26bcd38fef5e2269ff1f204643
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-10-18 00:50:16 -0400

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  function timerFunction(){
+    const newTime = timeRemaining - 1
+    
+    if (newTime >0) {
+      setTimeRemaining(newTime)
+    } else {
+      console.log("out of time")
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+
+  }
+
+    // this code is without a cleanup function BJJ
+  // useEffect(
+  //   // 1st arg: side effect (callback function)
+  //   () => {
+  //     console.log("useffeectBen")
+  //     setTimeout(() => timerFunction(), 1000);
+  //     // 2nd arg: dependencies array
+  //   }
+  // );
+
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      timerFunction();
+    }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
